6dda7824311a2fd45017450f538149405a402212
Lucas Padovan <lucas.padovan.correa@alumni.usp.br>
2025-08-12 21:53:26 -0300

    Resolução do erro bin/rails, e price pra integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_09_153430) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_13_004326) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_09_153430) do
     t.string "name"
     t.string "category"
     t.string "brand"
-    t.float "price"
+    t.integer "price"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
